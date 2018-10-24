62603febb6fab270bf7cb802662208344ad3408d
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-10-24 17:59:12 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,9 @@
 # code your #position_taken? method here!
 
-def position_taken(board, num)
-  if board[num.to_i - 1] == "   "
+def position_taken?(board, num)
+  if board[num.to_i - 1].strip == ""
+    false
+  else
+    true
+  end
+end
